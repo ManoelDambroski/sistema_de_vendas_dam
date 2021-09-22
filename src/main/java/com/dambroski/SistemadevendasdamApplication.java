366878de13af92045bae895b2f9335d693cba1e7
ManoelDambroski <manoeldambroski@gmail.com>
2021-09-22 03:30:33 -0300

    Created project and add dependencies

---
+++ src/main/java/com/dambroski/SistemadevendasdamApplication.java
@@ -0,0 +1,13 @@
+package com.dambroski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadevendasdamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadevendasdamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dambroski/SistemadevendasdamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dambroski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadevendasdamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
